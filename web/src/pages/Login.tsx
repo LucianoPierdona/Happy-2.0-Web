6a155b9a3f1b470d0568e1d6bb00e66234df10fe
Luciano Pierdona <lucianompj1@gmail.com>
2020-10-28 15:29:37 -0300

    finished styling for the login page

--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -9,6 +9,8 @@ import { config } from "../utils/config";
 import logoImg from "../assets/Logotipo.svg";
 
 import "../styles/pages/login.css";
+import { Link } from "react-router-dom";
+import { FiArrowLeft } from "react-icons/fi";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -38,6 +40,9 @@ const Login = () => {
 
   return (
     <>
+      <Link to="/" className="go-back">
+        <FiArrowLeft color="#15C3D6" size={20} />
+      </Link>
       <div className="banner-left">
         <img src={`${logoImg}`} alt="logo Happy" />
         <div className="location">
@@ -47,21 +52,29 @@ const Login = () => {
       </div>
       <div className="banner-right">
         <form>
+          <h1>Fazer Login!</h1>
+          <label htmlFor="email">E-Mail</label>
           <input
             type="email"
             name="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            placeholder="Email"
           />
+          <label htmlFor="password">Senha</label>
           <input
             type="password"
             name="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            placeholder="password"
           />
-          <button type="submit" onClick={onFormSubmit}>
+          <div className="subtitles">
+            <div>
+              <input type="checkbox" name="remember" />
+              <label htmlFor="remember">Lembrar-me</label>
+            </div>
+            <Link to="/">Esqueci a Senha</Link>
+          </div>
+          <button type="submit" disabled onClick={onFormSubmit}>
             Enviar
           </button>
         </form>
